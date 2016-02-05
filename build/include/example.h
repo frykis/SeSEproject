0d31304d0790c5f06869ced72ce2f9903649d852
Fredrik Fryklund <fryklund.fredrik@gmail.com>
2016-02-05 16:03:19 +0100

    Removed build

--- build/include/example.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXAMPLE_H_INCLUDED
-#define EXAMPLE_H_INCLUDED
-double example_FFT_ones(int NUM_POINTS);
-void example_FFT_manipulate_list(double coeff[]);
-void example_FFT_manipulate_matrix(double** coeff);
-void example_FFT_signal(int NUM_POINTS, double** coeff);
-#endif
-
