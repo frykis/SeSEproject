e17b9a7715a7aabb74777226e54a393aa1612407
Fredrik Fryklund <fryklund.fredrik@gmail.com>
2016-02-05 15:40:25 +0100

    Added build directory as Travis needs it

---
+++ build/include/example.h
@@ -0,0 +1,8 @@
+#ifndef EXAMPLE_H_INCLUDED
+#define EXAMPLE_H_INCLUDED
+double example_FFT_ones(int NUM_POINTS);
+void example_FFT_manipulate_list(double coeff[]);
+void example_FFT_manipulate_matrix(double** coeff);
+void example_FFT_signal(int NUM_POINTS, double** coeff);
+#endif
+
